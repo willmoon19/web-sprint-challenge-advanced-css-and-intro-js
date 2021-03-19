08c92f2798631894cfe5683f6ce8f8418827065e
William Moon <william-moon@lambdastudents.com>
2021-03-19 10:41:03 -0700

    finished task 3

--- index.js
+++ index.js
@@ -229,8 +229,8 @@ console.log(artists[8]);
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, number) {
+  return `the artist at index ${array[number].id} is ${array[number].name}`;
 }  
 
 
