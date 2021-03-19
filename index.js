760625cbd7179eb7f52fcce66e70c5838ec568a2
William Moon <william-moon@lambdastudents.com>
2021-03-19 11:33:58 -0700

    finished task 4 & 5

--- index.js
+++ index.js
@@ -243,8 +243,17 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].years.includes("1907 - 1954")){
+    newArray.push(array[i].name);
+    }
+    if (array[i].years.includes("1904 - 1989")){
+      newArray.push(array[i].name);
+    }
+  }
+  return newArray;
 }
 
 
@@ -258,8 +267,9 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, number){
+   let removed = array.splice(number, 1);
+   return array.length;
 }
    
 
